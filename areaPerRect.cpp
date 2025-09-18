39cced994ed0b8499ccf6842122127563d83a25e
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-09-18 21:13:11 +0000

    Area and Perimeter of a rectangle cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 Yoma Ozoh All rights reserved
+//
+// Created by Yoma Ozoh
+// Date: September 18, 2025
+// This program calculates Area and Perimeter of a rectangle
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "7cm x 4cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " <<(7*4) << "cm^2" << std::endl;
+    std::cout << "Perimeter is: " << (2*(7+4)) << "cm" << std::endl;
+}
